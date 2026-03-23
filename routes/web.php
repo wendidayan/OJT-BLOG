d6bd39ae66074fea5b853b497c186b59b9ed68cf
meow <161179908+wendidayan@users.noreply.github.com>
2026-03-23 09:09:51 +0800

    correct order

--- routes/web.php
+++ routes/web.php
@@ -129,6 +129,9 @@ Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->middleware('ad
 Route::get('/docs', function () {
     return response()->json(\App\Models\Blog::whereNotNull('documentation_images')
         ->where('documentation_images', '!=', '[]')
+        ->orderByRaw("NULLIF(regexp_replace(task, '[^0-9]', '', 'g'), '')::int asc nulls last")
+        ->orderBy('date_from')
+        ->orderBy('id')
         ->get(['id', 'task', 'week', 'date_from', 'date_to', 'featured_image', 'documentation_images']));
 });
 
